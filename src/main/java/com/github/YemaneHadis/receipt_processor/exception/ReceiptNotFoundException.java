742e94566b97040624e504ddc1a1e8d360cc4eff
yemane <yemane.hadis@gmail.com>
2024-09-22 15:49:28 -0400

    Adding Custom Exception for 404

---
+++ src/main/java/com/github/YemaneHadis/receipt_processor/exception/ReceiptNotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.YemaneHadis.receipt_processor.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ReceiptNotFoundException extends Exception{
+}
